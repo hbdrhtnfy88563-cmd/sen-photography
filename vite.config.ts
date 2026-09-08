7bdd3bc02142dd2eb1cd8e3d82a05968fb1f5d96
hbdrhtnfy88563-cmd <hbdrhtnfy88563@gmail.com>
2026-09-08 21:17:39 +0530

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import {defineConfig} from 'vite';
-base: '/sen-photography/',
+
 export default defineConfig(() => {
   return {
     plugins: [react(), tailwindcss()],
